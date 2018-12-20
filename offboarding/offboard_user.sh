63f0fd8abb3c67337631648017fe63cc00547cc6
jacobrian <jacobrian@gmail.com>
2018-12-19 19:29:33 -0800

    logloc

--- offboarding/offboard_user.sh
+++ offboarding/offboard_user.sh
@@ -1,65 +1,66 @@
-#!/bin/bash
 #This script performs various offboarding tasks when a user leaves the company.
 username=$1
-gam="$HOME/bin/gam/gam"
 
-echo Offboard executed by $(whoami) on $(date) | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+logloc="ENTER LOG PATH HERE"
+logit="tee -a $logloc"
+
+echo Offboard executed by $(whoami) on $(date) | $logit
 
 # Removing all mobile devices connected
-echo "Gathering mobile devices for $username" | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+echo "Gathering mobile devices for $username" | $logit
 IFS=$'\n'
-mobile_devices=($($gam print mobile query $username | grep -v resourceId | awk -F"," '{print $1}'))
+mobile_devices=($(gam print mobile query $username | grep -v resourceId | awk -F"," '{print $1}'))
 unset IFS
 	for mobileid in ${mobile_devices[@]}
 		do
-			$gam update mobile $mobileid action account_wipe && echo "Removing $mobileid from $username"
-	done | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+			gam update mobile $mobileid action account_wipe && echo "Removing $mobileid from $username"
+	done | $logit
 
 # Changing user's password to random
 echo "Changing "$username"'s' password to something random"
-$gam update user $username password random | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+gam update user $username password random | $logit
 
 # Removing all App-Specific account passwords, deleting MFA Recovery Codes,
 # deleting all OAuth tokens
 echo "Checking and Removing all of "$username"'s Application Specific Passwords, 2SV Recovery Codes, and all OAuth tokens"
-$gam user $username deprovision | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+gam user $username deprovision | $logit
 
 # Removing user from all Groups
 echo "Gathering group information for $username"
-amount_of_groups="$($gam info user $username | grep "Groups: (" | sed 's/[^0-9]//g')"
+amount_of_groups="$(gam info user $username | grep "Groups: (" | sed 's/[^0-9]//g')"
 IFS=$'\n'
-groups_list=($($gam info user $username | grep -A $amount_of_groups Groups | grep -v Groups | sed 's/^[^<]*<//g' | sed 's/\@.*$//g'))
+groups_list=($(gam info user $username | grep -A $amount_of_groups Groups | grep -v Groups | sed 's/^[^<]*<//g' | sed 's/\@.*$//g'))
 unset IFS
 	for group_name in ${groups_list[@]}
 		do
-			$gam update group $group_name remove user $username && echo "Removed $username from $group_name"
-	done | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+			gam update group $group_name remove user $username && echo "Removed $username from $group_name"
+	done | $logit
 
 # Forcing change password on next sign-in and then disabling immediately.
 # Speculation that this will sign user out within 5 minutes and not allow
 # user to send messages without reauthentication
 echo "Setting force change password on next logon and then disabling immediately to expire current session"
-$gam update user $username changepassword on
+gam update user $username changepassword on
 sleep 2 && echo "Waiting for 2 seconds"
-$gam update user $username changepassword off
+gam update user $username changepassword off
 
 # Generating new set of MFA recovery codes for the user
 echo "Generating new 2SV Recovery Codes for $username"
-$gam user $username update backupcodes | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+gam user $username update backupcodes | $logit
 
 # Removing all of user's calendar events
 read -r -p "Do you want to Wipe "$username"'s calendar? [Yes/No] " wipe_answer
 if [[ $wipe_answer =~ ^([yY][eE][sS]|[yY][eE]|[yY])+$ ]]
 	then
 		echo "Deleting all of "$username"'s calendar events"
-		$gam calendar $username wipe | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+		gam calendar $username wipe | $logit
 	else
-		echo "Not wiping calendar" | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+		echo "Not wiping calendar" | $logit
 fi
 
 # Suspending user
-echo "Setting $username to suspended" | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
-$gam update user $username suspended on | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+echo "Setting $username to suspended" | $logit
+gam update user $username suspended on | $logit
 
 # Asks admin if they want to transfer docs to manager, if so, asks for manager's
 # google username and then initiate a gdrive file transfer
@@ -68,9 +69,9 @@ read -r -p "Do you want to transfer Google Drive to the manager $r_manager? [Yes
 if [[ $response =~ ^([yY][eE][sS]|[yY][eE]|[yY])+$ ]]
 	then
 		echo "Creating transfer to $r_manager"
-		$gam create datatransfer $username gdrive $r_manager privacy_level shared,private | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+		gam create datatransfer $username gdrive $r_manager privacy_level shared,private | $logit
 	else
-		echo "Not transferring GDrive" | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+		echo "Not transferring GDrive" | $logit
 fi
 
 
@@ -80,17 +81,18 @@ read -r -p "Do you want to forward incoming email to the manager $r_manager? [Ye
 if [[ $response =~ ^([yY][eE][sS]|[yY][eE]|[yY])+$ ]]
         then
                 echo "Creating forwarding to $r_manager"
-                $gam update user $username email s_$username | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
-                $gam delete alias $username | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
-                $gam create group $username | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+                gam update user $username email s_$username | $logit
+                gam delete alias $username | $logit
 		sleep 10
-                $gam update group $username add owner user $r_manager | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
-                $gam update group $username show_in_group_directory false include_in_global_address_list false allow_web_posting false is_archived false who_can_view_group all_members_can_view | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+                gam create group $username description suspended_user | $logit
+								sleep 10
+                gam update group $username add owner user $r_manager | $logit
+                gam update group $username show_in_group_directory false include_in_global_address_list false allow_web_posting false is_archived false who_can_view_group all_members_can_view | $logit
         else
-                echo "Not setting up the forwarding" | tee -a "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log"
+                echo "Not setting up the forwarding" | $logit
 fi
 
 
 # Printing Log location
 echo "Offboard complete for $username."
-echo "Log located at "/Volumes/GoogleDrive/Team Drives/ITOps Team Drive/Offboarding/google_suite/$username.log""
+echo "Log located at $logloc"
